5d3f3e8a9fb0c158adbbd41760c464a75cce2d86
Eduardo Breda <edu12561@hotmail.com>
2021-05-26 12:36:36 -0300

    create a getScore function

--- ValidaçãoNotaEscolar/style.js
+++ ValidaçãoNotaEscolar/style.js
@@ -10,27 +10,41 @@ Crie um algoritmo que transforme as notas do sistemas numérica para sistema de 
 
 */
 
-let score = 90
-let scoreA = score >= 90 && score <= 100
-let scoreB = score >= 80 && score <= 89
-let scoreC = score >= 70 && score <= 79
-let scoreD = score >= 60 && score <= 69
-let scoreE = score < 60 && score >= 0
+function getScore(score) {
+    let scoreA = score >= 90 && score <= 100
+    let scoreB = score >= 80 && score <= 89
+    let scoreC = score >= 70 && score <= 79
+    let scoreD = score >= 60 && score <= 69
+    let scoreE = score < 60 && score >= 0
+    
+    let scoreFinal;
+    
+    if (scoreA) {
+        scoreFinal = "A"
+    } else if (scoreB) {
+        scoreFinal = "B"
+    } else if (scoreC) {
+        scoreFinal = "C"
+    } else if (scoreD) {
+        scoreFinal = "D"
+    } else if (scoreE) {
+        scoreFinal = "E"
+    }else {
+        scoreFinal = "Nota inválida"
+    }    
 
-let scoreFinal;
+    return scoreFinal
 
-if (scoreA) {
-    scoreFinal = "A"
-} else if (scoreB) {
-    scoreFinal = "B"
-} else if (scoreC) {
-    scoreFinal = "C"
-} else if (scoreD) {
-    scoreFinal = "D"
-} else if (scoreE) {
-    scoreFinal = "E"
-}else {
-    scoreFinal = "Nota inválida"
 }
 
-console.log(scoreFinal)
+console.log(getScore(101));
+console.log(getScore(-5));
+console.log(getScore(20));
+console.log(getScore(35));
+console.log(getScore(54));
+console.log(getScore(81));
+console.log(getScore(61));
+console.log(getScore(-120));
+console.log(getScore(26));
+console.log(getScore(11));
+console.log(getScore(84));
